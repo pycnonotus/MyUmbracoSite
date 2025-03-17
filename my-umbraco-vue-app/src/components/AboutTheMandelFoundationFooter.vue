baf788a065f5b45831d95e4ab456ad0605ac4d48
Anton G <anton.golo.fh@gmail.com>
2025-03-18 00:17:53 +0200

    more file changes

--- my-umbraco-vue-app/src/components/AboutTheMandelFoundationFooter.vue
+++ my-umbraco-vue-app/src/components/AboutTheMandelFoundationFooter.vue
@@ -3,11 +3,19 @@
 </script>
 
 <template>
-  <div class="pt-8 px-5 pb-6 bg-white">
-  <h2 class="font-medium font-masada">
-    העולם הבא מתחיל בעצמו ובעצמו יש להתמודד עם העולם הזה כדי להיות מעולם בעצמו
-  </h2>
-  </div>
+  <section class="w-full max-w-[100vw] pt-24.25 lg:pt-18.75 pb-5.5 lg:pb-8.75 relative overflow-hidden bg-theme-white">
+    <div class="container px-5 lg:px-11 max-w-[99.5rem]">
+      <div class="text-center text-base/[1.2] lg:text-[1.75rem]/[1.2] text-theme-blue-400"><h2
+          class="text-[2.5rem]/[1.1] lg:text-[5rem]/[1] font-medium font-masada mb-2 lg:mb-4.75">אודות קרן
+        מנדל-ישראל</h2>
+        <p>קרן מנדל-ישראל פועלת למען קידומה של ישראל יהודית ודמוקרטית, שוויונית, הגונה ופלורליסטית.<br>באמצעות הכשרת
+          מנהיגים ומנהיגות מונחי חזון לתחומי החינוך, החברה והתרבות.</p>
+        <footer class="pt-6.25 lg:pt-8"><a href="#"
+                                           class="inline-flex font-bold hover:underline font-bold text-main-blue-700 text-base/[1.2] lg:text-[1.75rem]/[1.2857142857]">אודות
+          הקרן &gt;</a></footer>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
