7ff7f3118c2aed2e050141c69e8a951034c5ae8e
Anton G <anton.golo.fh@gmail.com>
2025-03-23 00:45:07 +0200

    css fixes

--- my-umbraco-vue-app/src/components/AxSlidingTabs/Component/AxSlidingTabsItemActive.vue
+++ my-umbraco-vue-app/src/components/AxSlidingTabs/Component/AxSlidingTabsItemActive.vue
@@ -1,90 +1,25 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <h3 class="text-[#04283E] text-right font-['SimplerPro',sans-serif] font-bold leading-[110%] text-[18px] lg:text-[28px] lg:leading-[36px]">
+      {{ props.item.title }}
+    </h3>
+    <p class="text-[#04283E] text-right font-['SimplerPro',sans-serif] font-normal leading-[120%] text-[16px] lg:text-[28px]">
+      {{ props.item.description }}
+    </p>
+    <a
+        :href="props.item.link[0].url"
+        class="!text-[#04283E] text-right font-['SimplerPro',sans-serif] font-bold leading-[120%] text-[16px] opacity-50 lg:text-[28px] lg:opacity-100"
+    >
+      {{ props.item.link[0].title }}&nbsp;&nbsp;&gt;
+    </a>
+  </div>
+</template>
+
 <script setup lang="ts">
-import {defineProps} from "vue";
-import type {TabItemProperties} from "../Interfaces/IUmbracoData.ts";
+import { defineProps } from "vue";
+import type { TabItemProperties } from "../Interfaces/IUmbracoData.ts";
 
 const props = defineProps<{
   item: TabItemProperties;
 }>();
 </script>
-
-<template>
-  <div>
-    <h3>{{ props.item.title }}</h3>
-    <p>
-      {{ props.item.description }}
-    </p>
-    <a :href="props.item.link[0].url">
-      {{ props.item.link[0].title }}&nbsp;&nbsp;>
-    </a>
-  </div>
-</template>
-
-<style scoped>
-* {
-  transition: all 0.3s ease-in-out;
-}
-
-@media (min-width: 991px) {
-  h3 {
-    color: #04283E;
-    text-align: right;
-    font-family: "SimplerPro", sans-serif;
-    font-size: 28px !important;
-    font-style: normal;
-    font-weight: 700 !important;
-    line-height: 36px !important;
-  }
-
-  p {
-
-    font-size: 28px !important;
-    font-style: normal;
-    font-weight: 400 !important;
-    line-height: 120% !important;
-  }
-  a{
-    opacity: 1 !important;
-    font-size: 28px !important;
-    font-style: normal !important;
-    font-weight: 700 !important;
-    line-height: 120% !important;
-  }
-}
-
-div {
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-}
-
-h3 {
-  color: #04283E;
-  text-align: right;
-  font-family: "SimplerPro", sans-serif;
-  font-size: 18px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 110%; /* 19.8px */
-}
-
-p {
-  color: #04283E;
-  text-align: right;
-  font-family: "SimplerPro", sans-serif;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 120%; /* 19.2px */
-}
-
-a {
-  color: #04283E;
-  text-align: right;
-  font-family: "SimplerPro", sans-serif;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 120%; /* 19.2px */
-  opacity: 0.5;
-}
-</style>
--- my-umbraco-vue-app/src/components/AxSlidingTabs/Component/AxSlidingTabsItemNotActive.vue
+++ my-umbraco-vue-app/src/components/AxSlidingTabs/Component/AxSlidingTabsItemNotActive.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="custom-header">
-    <h3 class="text-[#04283E] text-right font-[SimplerPro,sans-serif] text-lg font-bold leading-[110%] opacity-50 lg:text-[28px] lg:leading-[36px]">
+    <h3 class="text-[#04283E] text-[16px] text-right font-[SimplerPro,sans-serif] text-lg font-bold leading-[110%] opacity-50 lg:text-[28px] lg:leading-[36px]">
       {{ props.title }}
     </h3>
   </div>
