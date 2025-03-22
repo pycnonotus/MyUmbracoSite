4ceb79ab73e9ddbb054b2bacad6af3a92723cdb9
Anton G <anton.golo.fh@gmail.com>
2025-03-23 01:08:40 +0200

    css fixes

--- my-umbraco-vue-app/src/components/AxSlidingTabs/Component/AxSlidingTabsItem.vue
+++ my-umbraco-vue-app/src/components/AxSlidingTabs/Component/AxSlidingTabsItem.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import {defineProps} from 'vue';
-import type {TabItemProperties} from "../Interfaces/IUmbracoData.ts";
+import { defineProps } from 'vue';
+import type { TabItemProperties } from "../Interfaces/IUmbracoData.ts";
 import AxSlidingTabsItemActive from "./AxSlidingTabsItemActive.vue";
 import AxSlidingTabsItemNotActive from "./AxSlidingTabsItemNotActive.vue";
 import AxSlidingTabsItemProgressBar from "./AxSlidingTabsItemProgressBar.vue";
@@ -16,7 +16,8 @@ const props = defineProps<{
   item: TabItemProperties;
 }>();
 
-function onButtonPressed() {
+function onButtonPressed(e: Event) {
+  e.stopPropagation();
   emit('button-pressed');
 }
 
@@ -24,85 +25,27 @@ function onTitlePressed() {
   console.log('onTitlePressed');
   emit('title-pressed');
 }
-
 </script>
 
 <template>
-  <div class="tab" @click="onTitlePressed">
-    <div class="progress-bar-container">
+  <div @click="onTitlePressed" class="flex gap-7 lg:flex-col">
+    <!-- Progress Bar Container -->
+    <div class="flex flex-col gap-2.5 items-center w-6 cursor-pointer lg:flex-row lg:w-full lg:h-12 lg:gap-6">
       <button
           v-if="props.isActive"
-          @click="onButtonPressed"
+          @click.stop="onButtonPressed"
+          class="flex items-center justify-center w-6 h-6 lg:w-12 lg:h-12 !p-0 !m-0 border-0 bg-transparent appearance-none"
       >
-        <img src="/src/assets/images/pause.svg" loading="lazy" alt="pause">
+        <img
+            src="/src/assets/images/pause.svg"
+            loading="lazy"
+            alt="pause"
+            class="w-full h-full object-cover block"
+        />
       </button>
-      <AxSlidingTabsItemProgressBar :progress="props.isActive ? props.progress : 0"/>
+      <AxSlidingTabsItemProgressBar :progress="props.isActive ? props.progress : 0" class="grow" />
     </div>
-    <AxSlidingTabsItemActive v-if="props.isActive" :item="props.item"/>
-    <AxSlidingTabsItemNotActive v-else :title="props.item.title"/>
+    <AxSlidingTabsItemActive v-if="props.isActive" :item="props.item" />
+    <AxSlidingTabsItemNotActive v-else :title="props.item.title" />
   </div>
 </template>
-
-<style scoped>
-
-@media (min-width: 991px) {
-  .tab {
-    flex-direction: column;
-  }
-
-  .progress-bar-container {
-    flex-direction: row !important;
-    width: 100% !important;
-    height: 48px !important;
-    gap: 24px !important;
-  }
-
-  button {
-    width: 48px !important;
-    height: 48px !important;
-  }
-
-}
-
-button {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 24px;
-  height: 24px;
-  padding: 0;
-  flex-shrink: 0;
-}
-
-
-img {
-  width: 100%;
-  height: 100%;
-}
-
-button {
-  border: none;
-  background-color: transparent;
-}
-
-
-AxSlidingTabsItemProgressBar {
-  flex-grow: 1;
-}
-
-.tab {
-  display: flex;
-  gap: 28px;
-}
-
-.progress-bar-container {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  align-items: center;
-  width: 24px;
-  cursor: pointer;
-}
-
-
-</style>
