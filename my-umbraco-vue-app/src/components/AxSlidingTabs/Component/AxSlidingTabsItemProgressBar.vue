73e27c006a9ed2753877237fa709feb3a0f00f06
Anton G <anton.golo.fh@gmail.com>
2025-03-23 00:24:04 +0200

    change css to tail wind

--- my-umbraco-vue-app/src/components/AxSlidingTabs/Component/AxSlidingTabsItemProgressBar.vue
+++ my-umbraco-vue-app/src/components/AxSlidingTabs/Component/AxSlidingTabsItemProgressBar.vue
@@ -1,52 +1,18 @@
-<script setup lang="ts">
-import {defineProps} from "vue";
-
-const props = defineProps<{
-  progress: number;
-}>();
-
-</script>
-
 <template>
-  <div
-      class="progress-bar"
-  >
+  <div class="basis-[3px] bg-[#9D57264C] grow overflow-hidden rounded-full flex transition-all duration-300 ease-in-out">
     <div
-        class="progress-bar-inner"
         :style="{ '--progress': `${props.progress}%` }"
+        class="bg-[#9D5726] rounded-full transition-[height] duration-[10ms] ease-linear
+             w-[3px] h-[var(--progress)]
+             lg:h-[6px] lg:w-[var(--progress)]"
     ></div>
   </div>
 </template>
 
-<style scoped>
-:host {
-  --progress: 0;
-}
+<script setup lang="ts">
+import { defineProps } from "vue";
 
-.progress-bar {
-  flex-basis: 3px;
-  background-color: #9D57264C;
-  flex-grow: 1;
-  overflow: hidden;
-  border-radius: 999px;
-  display: flex;
-  transition: all 0.3s ease-in-out;
-}
-
-@media (min-width: 991px) {
-  .progress-bar-inner {
-    flex-direction: column;
-    height: 6px !important;
-    width: var(--progress) !important;
-  }
-}
-
-
-.progress-bar-inner {
-  width: 3px;
-  background-color: #9D5726;
-  border-radius: 999px;
-  height: var(--progress);
-  transition: height 0.01s linear;
-}
-</style>
+const props = defineProps<{
+  progress: number;
+}>();
+</script>
