a054a147e8f3cef9c376985bd1772062992b7dd2
Anton G <anton.golo.fh@gmail.com>
2025-03-23 00:34:29 +0200

    css fixes

--- my-umbraco-vue-app/src/components/AxSlidingTabs/Component/AxSlidingTabsItemNotActive.vue
+++ my-umbraco-vue-app/src/components/AxSlidingTabs/Component/AxSlidingTabsItemNotActive.vue
@@ -1,39 +1,13 @@
-<script setup lang="ts">
-import {defineProps} from "vue";
-const props = defineProps<{
-  title: string;
-}>();
-</script>
-
 <template>
-<div>
-  <h3>{{ props.title }}</h3>
-</div>
+  <div class="custom-header">
+    <h3 class="text-[#04283E] text-right font-[SimplerPro,sans-serif] text-lg font-bold leading-[110%] opacity-50 lg:text-[28px] lg:leading-[36px]">
+      {{ props.title }}
+    </h3>
+  </div>
 </template>
 
-<style scoped>
-h3 {
-  color: #04283E;
-  text-align: right;
-  font-family: "SimplerPro", sans-serif;
-  font-size: 18px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 110%; /* 19.8px */
-  opacity: 0.5;
-}
+<script setup lang="ts">
+import { defineProps } from "vue";
+const props = defineProps<{ title: string }>();
+</script>
 
-@media (min-width: 991px) {
-  h3 {
-    color: #04283E;
-    text-align: right;
-    font-family: "SimplerPro", sans-serif;
-    font-size: 28px !important;
-    font-style: normal;
-    font-weight: 700 !important;
-    line-height: 36px !important;
-  }
-}
-
-
-</style>
