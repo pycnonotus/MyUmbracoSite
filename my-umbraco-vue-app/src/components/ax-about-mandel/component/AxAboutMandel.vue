fc5b1720786d5eac5b3429c832f2423dc7f5257e
Anton G <anton.golo.fh@gmail.com>
2025-03-24 06:51:53 +0200

    trnslate to tailwuind

--- my-umbraco-vue-app/src/components/ax-about-mandel/component/AxAboutMandel.vue
+++ my-umbraco-vue-app/src/components/ax-about-mandel/component/AxAboutMandel.vue
@@ -1,41 +1,24 @@
 <script setup lang="ts">
-import type {AboutMandelFooterItemProperties, IAxAboutMandel} from "../interfaces/IAxAboutMandel.ts";
+import type { AboutMandelFooterItemProperties, IAxAboutMandel } from "../interfaces/IAxAboutMandel.ts";
 import AxAboutMandelInformation from "./AxAboutMandelInformation.vue";
 import AxAboutMandelUnits from "./AxAboutMandelUnits.vue";
-import {computed} from "vue";
-import type {ComputedRef} from "@vue/reactivity";
+import { computed } from "vue";
+import type { ComputedRef } from "@vue/reactivity";
 
 const props = defineProps<{
   item: IAxAboutMandel;
 }>();
 
-const units: ComputedRef<AboutMandelFooterItemProperties[]> = computed(() => props.item.properties.units.items.map((item) => item.content.properties));
-
+const units: ComputedRef<AboutMandelFooterItemProperties[]> = computed(() =>
+    props.item.properties.units.items.map((item) => item.content.properties)
+);
 </script>
 
 <template>
-  <section>
-    <div class="about-mandel">
+  <section class="w-full max-w-[120rem] mx-auto">
+    <div class="flex flex-col items-center justify-center gap-[3.6875rem]">
       <AxAboutMandelInformation :item="props.item" />
       <AxAboutMandelUnits :item="units" />
     </div>
   </section>
 </template>
-
-<style scoped>
-section {
-  width: 100%;
-  max-width: 1920px;
-
-  margin: 0 auto;
-}
-
-.about-mandel {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 59px;
-}
-
-</style>
